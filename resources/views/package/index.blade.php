732f9ab14b28fd442a5800650e4bceede3261856
RAT JANTARAKSA <teepuxdev@gmail.com>
2018-08-04 20:15:57 +0700

    add history count

--- resources/views/package/index.blade.php
+++ resources/views/package/index.blade.php
@@ -27,7 +27,7 @@
                             {{ ($package->date_stop) ? $package->date_stop->format('d/m/Y') : '' }}
                         </td>
                         <td>
-                            <a href="{{ route('package.edit', $package->id) }}" class="btn btn-info btn-sm">edit</a>
+                            <span class="badge badge-danger">{{ $package->histories()->count() }}</span>
                         </td>
                     </tr>
                     @endforeach
